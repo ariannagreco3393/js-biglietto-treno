64e07cb6319c80138c9662e6d7ec4d83d1d6acdb
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-24 16:04:37 +0100

    edit const

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,35 +15,34 @@ const km_da_percorrere =  Number(prompt("Quanti km devi percorrere?"))
 const eta_passeggero =  Number(prompt("Quanti anni hai?"))
 
 //calcolare prezzo totale del viaggio 0.21€ al km
-const price_ticket = (km_da_percorrere * 0.21);
+let price_ticket = (km_da_percorrere * 0.21);
 
 //sconto 20% se passeggero < 18 anni
 const under_18_discount = (price_ticket / 100 * 20)
 
 // prezzo scontato under 18
-const price_under_18_ticket = price_ticket - under_18_discount;
+let price_under_18_ticket = price_ticket - under_18_discount;
 
 //sconto 40% se passeggero > 65
 const over_65_discount = (price_ticket / 100 * 40)
 
 // prezzo scontato over 65
-const price_over_65_ticket = price_ticket - over_65_discount;
+let price_over_65_ticket = price_ticket - over_65_discount;
 
 //prezzo biglietto passeggero
 
 if (eta_passeggero < 18) {
-    const price_under_18_ticket = price_ticket - under_18_discount;
+    price_under_18_ticket = price_ticket - under_18_discount;
   
 } else if (eta_passeggero > 65) {
-    const price_over_65_ticket = price_ticket - over_65_discount;
+    price_over_65_ticket = price_ticket - over_65_discount;
     
 } else {
-    const price_ticket = (km_da_percorrere * 0.21);
+    price_ticket = (km_da_percorrere * 0.21);
     
-} 
+}
 
 //comunichiamo prezzo biglietto passeggero
-
 
 if (eta_passeggero < 18) {
     console.log("Il tuo biglietto con sconto under 18 costa € " + price_under_18_ticket);
@@ -54,7 +53,6 @@ if (eta_passeggero < 18) {
 } else {
     console.log("il tuo biglietto costa € " + price_ticket)  
 } 
-
 
 
 
